fc4ba72ae4a6a9a3991fc6dee2e3ede4b1a3a5de
Joseph Benton <josephtbenton@gmail.com>
2017-02-15 22:21:49 -0600

    post first meeting

---
+++ Assets/Scripts/DockRock.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DockRock : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		// create player
+		// create list of enemies (randomly on dock)
+		// Start screen w/instructions
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+	public float easing = 1f;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+		
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 mousePos2D = Input.mousePosition;
+		mousePos2D.z = Camera.main.transform.position.z;
+		Vector3 mousePos3D = Camera.main.ScreenToWorldPoint (mousePos2D);
+
+		Vector3 pos = this.transform.position;
+		pos.x = mousePos3D.x;
+		pos.z = mousePos3D.z;
+		this.transform.position = Vector3.Lerp(this.transform.position, pos, easing);
+		
+	}
+}
+
